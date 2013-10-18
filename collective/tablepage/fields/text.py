010c97c7bb83406d09e4fbdfa96f60c693fbd832
Luca Fabbri <luca@keul.it>
2013-10-18 15:57:29 +0200

    Fixed a performance/security problem: data inside text cells were transformed to HTML without any check

--- collective/tablepage/fields/text.py
+++ collective/tablepage/fields/text.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from Products.CMFCore.utils import getToolByName
 from zope.interface import implements
 from collective.tablepage.interfaces import IColumnField
 from collective.tablepage.fields.base import BaseField
@@ -24,3 +25,10 @@ class TextAreaField(BaseField):
     edit_template = ViewPageTemplateFile('templates/textarea.pt')
     view_template = ViewPageTemplateFile('templates/textarea_view.pt')
 
+    def renderText(self, text):
+        context = self.context
+        transformer = getToolByName(context, 'portal_transforms')
+        data = transformer.convertTo('text/html', text,
+                                     context=context,
+                                     mimetype='text/plain')
+        return data.getData()
